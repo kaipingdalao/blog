22d9b8e80e411e1a39e444c7e587de7a87cd8dc6
kaipingdalao <1767917605@qq.com>
2020-08-27 17:20:07 +0800

    blog

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+    div#App
+        Header.header
+        router-view
+        Footer
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Header from './components/common/Header'
+    import Footer from './components/common/Footer'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Header,
+            Footer
+        },
+        mounted() {},
+        methods: {
+            timeToDate(time) {
+                var date = new Date(time * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+                var Y = date.getFullYear();
+                var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+                var D = date.getDate();
+                // var h = date.getHours() + ':';
+                // var m = date.getMinutes() + ':';
+                // var s = date.getSeconds();
+
+                const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+
+                return month[parseInt(M) - 1] + ' ' + D + ', ' + Y;
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import "../public/static/css/reset.css";
+
+    .header {
+        height: auto !important;
+    }
+
+    #App {
+        background-color: #f7f7f7 !important;
+    }
+    ::selection {
+        color: #fff;
+        background: #088;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import global_ from './lib/Global.vue'
+import Bus from './lib/eventBus'
+import req from './lib/request'
+import headroom from 'vue-headroom'
+
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+// 本身样式 当组件本身请求样式请求不成功时使用备份样式
+import '../public/static/mavon-deitor1/github.min.css'
+import '../public/static/mavon-deitor1/github-markdown.min.css'
+import '../public/static/mavon-deitor1/katex.min.css'
+// import '../public/static/mavon-deitor1/katex.min'
+// 自定义样式 覆盖 mavon-editor 本身样式
+import '../public/static/css/mk.less'
+
+Vue.use(headroom)
+Vue.use(mavonEditor)
+
+// axios
+Vue.prototype.GLOBAL = global_;
+axios.defaults.baseURL=global_.BASE_URL;
+Vue.prototype.$axios = axios;
+
+Vue.prototype.$request = req
+
+// 在组件中已 this.$Bus 调用
+Vue.prototype.$Bus = Bus;
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router: router,
 }).$mount('#app')
+
